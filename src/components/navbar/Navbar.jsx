ac6080e3870775d2d91c0ebc078d328b4ab0911d
sistacuy-2022017 <sistacuy-2022017@kinal.edu.gt>
2024-07-03 23:00:06 -0600

    subiendo nav responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,64 +12,73 @@ export const Navbar = () => {
 
     return (
         <nav className="bg-blue-500 p-4 shadow-md">
-            <div className="container mx-auto flex justify-between items-center">
-                {/* Logo and Search */}
-                <div className="flex items-center space-x-4">
-                    <img
-                        src={Logo}
-                        alt="DUA"
-                        className="h-12"
-                    />
-                    <div className="relative">
+            <div className="container mx-auto">
+                <div className="flex justify-between items-center">
+                    {/* Logo */}
+                    <Link to="/" className="flex items-center">
+                        <img src={Logo} alt="DUA" className="h-10 md:h-12" />
+                    </Link>
+
+                    {/* Search Bar - Hidden on mobile, visible on larger screens */}
+                    <div className="hidden md:flex relative flex-grow max-w-xl mx-4">
                         <input
                             type="text"
                             placeholder="Buscar productos, marcas y más..."
-                            className="p-2 pl-10 pr-4 w-72 rounded border border-gray-300 focus:outline-none focus:border-blue-600"
+                            className="p-2 pl-10 pr-4 w-full rounded border border-gray-300 focus:outline-none focus:border-blue-600"
                         />
                         <span className="absolute left-3 top-2 text-gray-400"><FiSearch className="h-5 w-5" /></span>
                     </div>
-                </div>
 
-                {/* Mobile Menu Button */}
-                <div className="flex lg:hidden items-center">
-                    <button
-                        onClick={toggleMenu}
-                        className="p-2 focus:outline-none"
-                    >
+                    {/* Mobile Menu Button */}
+                    <button onClick={toggleMenu} className="md:hidden p-2 focus:outline-none">
                         {isOpen ? <FiX className="h-6 w-6" /> : <FiMenu className="h-6 w-6" />}
                     </button>
+
+                    {/* Desktop Menu */}
+                    <div className="hidden md:flex items-center space-x-4">
+                        <Link to="/mi-cuenta" className="flex items-center space-x-1 p-2">
+                            <FiUser className="h-6 w-6" />
+                            <span>Mi cuenta</span>
+                        </Link>
+                        <Link to="/carrito" className="flex items-center space-x-1 p-2">
+                            <FiShoppingCart className="h-6 w-6" />
+                            <span>Carrito</span>
+                        </Link>
+                        <Link to="/ubicacion" className="flex items-center space-x-1 p-2">
+                            <FiMapPin className="h-6 w-6" />
+                            <span>Guatemala</span>
+                        </Link>
+                    </div>
                 </div>
 
-                {/* Desktop Menu */}
-                <div className={`lg:flex items-center space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
-                    <Link to="/mi-cuenta" className="flex items-center space-x-1 p-2">
-                        <FiUser className="h-6 w-6" />
-                        <span>Mi cuenta</span>
-                    </Link>
-                    <Link to="/carrito" className="flex items-center space-x-1 p-2">
-                        <FiShoppingCart className="h-6 w-6" />
-                        <span>Carrito</span>
-                    </Link>
-                    <Link to="/ubicacion" className="hidden lg:flex items-center space-x-1 p-2">
-                        <FiMapPin className="h-6 w-6" />
-                        <span>Guatemala</span>
-                    </Link>
+                {/* Mobile Menu */}
+                <div className={`md:hidden ${isOpen ? 'block' : 'hidden'} mt-4`}>
+                    <div className="flex flex-col space-y-2">
+                        <Link to="/mi-cuenta" className="flex items-center space-x-1 p-2">
+                            <FiUser className="h-6 w-6" />
+                            <span>Mi cuenta</span>
+                        </Link>
+                        <Link to="/carrito" className="flex items-center space-x-1 p-2">
+                            <FiShoppingCart className="h-6 w-6" />
+                            <span>Carrito</span>
+                        </Link>
+                        <Link to="/ubicacion" className="flex items-center space-x-1 p-2">
+                            <FiMapPin className="h-6 w-6" />
+                            <span>Guatemala</span>
+                        </Link>
+                        <div className="relative mt-2">
+                            <input
+                                type="text"
+                                placeholder="Buscar productos, marcas y más..."
+                                className="p-2 pl-10 pr-4 w-full rounded border border-gray-300 focus:outline-none focus:border-blue-600"
+                            />
+                            <span className="absolute left-3 top-2 text-gray-400"><FiSearch className="h-5 w-5" /></span>
+                        </div>
+                    </div>
                 </div>
-            </div>
 
-            {/* Bottom Navigation */}
-            <div className="mt-2 container mx-auto flex justify-between items-center text-black">
-                <div className="lg:hidden flex space-x-4">
-                    <a href="#" className="p-2">Categorías</a>
-                    <a href="#" className="p-2">Ofertas</a>
-                    <a href="#" className="p-2">Historial</a>
-                    <a href="#" className="p-2">Supermercado</a>
-                    <a href="#" className="p-2">Moda</a>
-                    <a href="#" className="p-2">Mercado Play</a>
-                    <a href="#" className="p-2">Vender</a>
-                    <a href="#" className="p-2">Ayuda</a>
-                </div>
-                <div className="hidden lg:flex space-x-4">
+                {/* Bottom Navigation */}
+                <div className="mt-4 flex flex-wrap justify-between items-center text-sm text-white">
                     <a href="#" className="p-2">Categorías</a>
                     <a href="#" className="p-2">Ofertas</a>
                     <a href="#" className="p-2">Historial</a>
